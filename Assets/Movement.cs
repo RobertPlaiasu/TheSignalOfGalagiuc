c1e00f28a0957b5c301d107bfd60f672738d901d
GrigoAleex <grigorescu.aleex@gmail.com>
2021-07-30 22:42:39 +0300

    Implemented Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed;
+    private bool isFacingRight = false;
+    private Rigidbody2D rb;
+    private Animator animator;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity =
+            new Vector2(horizontalInput * speed, verticalInput * speed);
+
+        if (
+            (isFacingRight && horizontalInput < 0) ||
+            (!isFacingRight && horizontalInput > 0)
+        ) Flip();
+
+        animator.SetBool("isMoving", CheckIsMoving());
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    bool CheckIsMoving()
+    {
+        if (rb.velocity.x == 0 && rb.velocity.y == 0) 
+            return false;
+        return true;
     }
 }
