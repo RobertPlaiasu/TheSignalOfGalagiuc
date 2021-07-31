9f4d52114c4381f875551b9e3661d5e758094267
GrigoAleex <grigorescu.aleex@gmail.com>
2021-07-31 12:41:04 +0300

    Connect to a server

---
+++ GameServer/GameServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.Title = "Game Server";
+            Console.WriteLine("Game Server");
+            
+            Server.Start(50,26950);
+            
+            Console.ReadKey();
+        }
+    }
+}
